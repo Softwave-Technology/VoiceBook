12b4786e03018fddfbf58b02f7a248df8909b7af
anilyavas <anil.yavas99@gmail.com>
2025-05-20 16:24:06 +0300

    Refactor TabLayout and add new tab

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,32 +1,55 @@
-import { Link, Tabs } from 'expo-router';
+import { Tabs } from 'expo-router';
 
-import { HeaderButton } from '../../components/HeaderButton';
 import { TabBarIcon } from '../../components/TabBarIcon';
+import { Pressable } from 'react-native';
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
 
 export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: 'black',
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: '#D4AF37',
+        tabBarInactiveTintColor: '#F5F5DC',
+        tabBarStyle: {
+          backgroundColor: '#1E1E1E',
+        },
       }}>
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Tab One',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-          headerRight: () => (
-            <Link href="/modal" asChild>
-              <HeaderButton />
-            </Link>
+          tabBarIcon: ({ color }) => <TabBarIcon name="book" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="new"
+        options={{
+          tabBarButton: (props) => (
+            <Pressable
+              {...props}
+              style={{
+                top: -20,
+                justifyContent: 'center',
+                alignItems: 'center',
+                backgroundColor: 'darkgreen',
+                shadowColor: '#fff',
+                shadowOffset: { width: 0.5, height: 0.5 },
+                shadowOpacity: 0.3,
+                shadowRadius: 5,
+                width: 70,
+                height: 70,
+                borderRadius: 35,
+                alignSelf: 'center',
+              }}>
+              <FontAwesome6 name="microphone" size={30} color="white" />
+            </Pressable>
           ),
         }}
       />
       <Tabs.Screen
-        name="two"
-        options={{
-          title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-        }}
+        name="options"
+        options={{ tabBarIcon: ({ color }) => <TabBarIcon name="gear" color={color} /> }}
       />
     </Tabs>
   );
